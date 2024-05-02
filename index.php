b2cf77ee2f372140c73968e096b3d1a4eb472d80
CatCoders1501 <119710236+CatCoders1501@users.noreply.github.com>
2024-05-01 18:51:27 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//Step1
+ $db = mysqli_connect('mysqlcluster5.registeredsite.com','rovadmin19','aqrw23Ds1','catcoderdb')
+ or die('Error connecting to MySQL server.');
+?>
+
+<html>
+ <head>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL</h1>
+ </body>
+</html>
