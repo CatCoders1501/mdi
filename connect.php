d3e76715094473355afbf56bd26ab498e032eda9
CatCoders1501 <119710236+CatCoders1501@users.noreply.github.com>
2024-05-02 17:18:13 -0700

    this works

--- connect.php
+++ connect.php
@@ -7,7 +7,7 @@
 	$number = $_POST['number'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('mysqlcluster5.registeredsite.com','rovadmin19','aqrw23Ds1','catcoderdb');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
